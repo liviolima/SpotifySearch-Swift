cd36880e3a14749599c80d19f73b4dcc0ca5293f
LivioLima <livio.lima.br@gmail.com>
2016-12-30 18:25:12 -0300

    shows albums on TableViewController.

--- SpotifySearch/ViewController.swift
+++ SpotifySearch/ViewController.swift
@@ -11,6 +11,8 @@ import Alamofire
 
 class TableViewController: UITableViewController {
     
+    var names = [String]()
+    
     var searchURL = "https://api.spotify.com/v1/search?q=Iron+Maiden&type=album"
 
     typealias JSONStandard = [String: AnyObject]
@@ -32,16 +34,41 @@ class TableViewController: UITableViewController {
     
     func parseData(JSONData : Data){
         do {
-            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as? JSONStandard
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
         
-            print(readableJSON)
+            if let albums = readableJSON["albums"] as? JSONStandard{
+                if let items = albums["items"]{
+                    
+                    for i in 0..<items.count{
+                        let item = items[i] as! JSONStandard
+
+                        let name = item["name"] as! String
+                        names.append(name)
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+           // print(readableJSON)
         }
         catch{
             print(error)
         }
         
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        
+        cell?.textLabel?.text = names[indexPath.row]
+        
+        return cell!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
